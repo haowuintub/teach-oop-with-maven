06f9b1d99567fa09ab2eb64be0fc0d882317b812
haowu_GitHub-Desktop <wu.haoingermany@gmail.com>
2021-04-27 16:27:43 +0200

    update MainUe.java for Uebung!

--- src/main/java/de/tuberlin/vsp/teach/geotools/MainUe.java
+++ src/main/java/de/tuberlin/vsp/teach/geotools/MainUe.java
@@ -12,19 +12,24 @@ import java.util.List;
 
 class MainUe {
 
-        public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {//throws IOException?
 
                 final String pathname = "data/shapefiles/thueringen-kreise.shp";
 
-                FileDataStore store = FileDataStoreFinder.getDataStore(new File(pathname));
+                FileDataStore store = FileDataStoreFinder.getDataStore(new File(pathname));//FileDataStoreFinder大写 直接用类 不用对象调用函数
 
                 FeatureReader<SimpleFeatureType, SimpleFeature> reader = store.getFeatureReader();
 
                 List<SimpleFeature> features = new ArrayList<>();
-                for (; reader.hasNext(); ) {
+                for (; reader.hasNext(); ) {//; reader.hasNext();可能是迭代器Iterator？
 
                         SimpleFeature result = reader.next();
                         features.add(result);
+//                        System.out.println(result.toString());
+//                        List<Object> attributes = result.getAttributes();//result.getAttributes()是一个数组?
+//                        for (Object o : result.getAttributes()) {//result.getAttributes()是一个数组?|为何不用attributes代替result.getAttributes()？
+//                                System.out.println(o.toString());
+//                        }
                 }
 
                 reader.close();
@@ -32,11 +37,30 @@ class MainUe {
                 System.out.println(features.size());
 
 
-                List<String> pointsString = new ArrayList<>();
-                pointsString.add("x coord, y coord");
+                List<String> pointsString = new ArrayList<>();//输入坐标准备
+                pointsString.add("x coord, y coord");//输入坐标第一行
 
 
-                for (SimpleFeature feature : features) {
+
+
+/*//              Aufgabe 1: die Mittelpunkt von 23 Bundeslander zu finden
+                for(SimpleFeature feature : features){
+                        double minX = feature.getBounds().getMinX();
+                        double maxX = feature.getBounds().getMaxX();
+                        double minY = feature.getBounds().getMinY();
+                        double maxY = feature.getBounds().getMaxY();
+                        double xNew = (minX+maxX)/2;
+                        double yNew = (minY+maxY)/2;
+
+//                        X coord, Y coord
+                        pointsString.add(xNew + "," + yNew);
+
+//                        System.out.println(minX);
+                }*/
+
+
+//                Aufgabe 2: in jeden Landkreis 5 random BounderBox erstellen
+                for(SimpleFeature feature : features){
                         double minX = feature.getBounds().getMinX();
                         double maxX = feature.getBounds().getMaxX();
                         double minY = feature.getBounds().getMinY();
@@ -52,5 +76,22 @@ class MainUe {
 
 
                 CSVTools.printCSV(pointsString, "data/coords.csv");
+
+
+
+
+                //                Point point = new GeometryFactory().createPoint(new Coordinate(xNew, yNew));
+
+
+/*                for (SimpleFeature feature : features) {
+                        BoundingBox bounds = feature.getBounds();
+                        Geometry polygon = (Geometry) feature.getAttributes().get(0);
+                        DirectPosition directPosition = new DirectPosition1D();
+                        if (polygon.contains(directPosition))
+                }*/
+
+
+
+
         }
 }
